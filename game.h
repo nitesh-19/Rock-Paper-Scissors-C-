92842ecf01e9333e79c60d83d9cdf465ab27e42b
nitesh-19 <niteshgohil25@gmail.com>
2023-11-09 21:43:07 +0530

    Added logic to get row modifiers

--- game.h
+++ game.h
@@ -15,19 +15,34 @@ using std::string;
 
 class RPS {
 private:
-    char array[4] = "RPS";
+    short array[9] = {-1, 0, 1, 1, -1, 0, 0, 1, -1}; //Decision Matrix 3x3
+    string tools[4] = {"Rock", "Paper", "Scissors"};
     char user_input;
-    short game_matrix[];
+    short index_row_modifier;
+
 
 public:
     void start_game();
-
 };
 
 void RPS::start_game() {
     int random_int = std::rand() % 3;
     std::cin >> user_input;
-    if(user_input == )
+
+    switch (user_input) {
+        case 'r' :
+            index_row_modifier = 0;
+            break;
+        case 'p' :
+            index_row_modifier = 3;
+            break;
+        case 's' :
+            index_row_modifier = 6;
+            break;
+    }
+    int result = array[index_row_modifier + random_int];
+    std::cout << "Computer Choice " << random_int << "\n";
+    std::cout << "Result " << result << "\n";
 
 
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include "game.h"
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    RPS my_game = RPS();
+    my_game.start_game();
+
 
     return 0;
 
